675cae2a366fb6c384a472885b1277be22e4bd8e
zakdok <zakdok@naver.com>
2020-07-24 16:55:14 +0900

    last lecture

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'c14c34dc6961a71e24ce3e08c2e59675';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
